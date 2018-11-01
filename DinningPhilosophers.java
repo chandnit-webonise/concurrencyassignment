4b0b11bbfc3141b516830ce491b6dae1dcdb3a12
Chandni <Chandni.tilani@weboniselab.com>
2018-11-01 19:46:03 +0530

    saving changes

--- DinningPhilosophers.java
+++ DinningPhilosophers.java
@@ -1,28 +1,21 @@
-
 public class DinningPhilosophers {
-
-	public static void main(String args[])
-	{
-		Philosopher philosopher[]=new Philosopher[5];
-		Object forks[]=new Object[philosopher.length];
-		
-		for(int i=0;i<forks.length;i++)
-		{
-			forks[i]=new Object();
-		}
-		
-		for (int i = 0; i < philosopher.length; i++) {
-            Object leftfork = forks[i];
-            Object rightfork = forks[(i + 1) % forks.length];
-            if(i==philosopher.length-1) {
-            	philosopher[i] = new Philosopher(rightfork, leftfork);
-            }
-            else {
-            	philosopher[i] = new Philosopher(leftfork, rightfork);
-            }  
-            Thread t 
-              = new Thread(philosopher[i], "Philosopher " + (i + 1));
-            t.start();
-		}
-	}
+  public static void main(String args[]) {
+    Philosopher philosopher[]=new Philosopher[5];
+    Object forks[]=new Object[philosopher.length];
+    for(int counter=0;counter<forks.length;counter++) {
+      forks[counter]=new Object();
+    }
+    for (int counter = 0; counter < philosopher.length; counter++) {
+      Object leftfork = forks[counter];
+      Object rightfork = forks[(counter + 1) % forks.length];
+        if(counter==philosopher.length-1) {
+        philosopher[counter] = new Philosopher(rightfork, leftfork);
+        }
+        else {
+        philosopher[counter] = new Philosopher(leftfork, rightfork);
+        }
+    Thread thread=new Thread(philosopher[counter], "Philosopher " + (counter + 1));
+    thread.start();
+    }
+  }
 }
